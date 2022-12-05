2859c13cb85782888da273e1fcdb0e3db9f5fdb2
Ekaterina <ekaterina.sementeva@gmail.com>
2022-12-05 16:02:22 -0500

    Rest Keycloak Security Integration

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,67 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping("/api/v1/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @GetMapping
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> getTasks(){
+        return ResponseEntity.ok(new ResponseWrapper("List of all tasks", taskService.listAllTasks(), HttpStatus.OK));
+    }
+
+    @GetMapping("/{id}")
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> getTaskById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(new ResponseWrapper("Retrieve task by id", taskService.findById(id), HttpStatus.OK));
+    }
+    @PostMapping
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> createTask(@RequestBody TaskDTO task){
+        taskService.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Task created", HttpStatus.CREATED));
+    }
+    @PutMapping
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> updateTask(@RequestBody TaskDTO task ){
+        taskService.update(task);
+        return ResponseEntity.ok(new ResponseWrapper("Task updated", HttpStatus.OK));
+    }
+    @DeleteMapping("/{id}")
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> deleteTask(@PathVariable("id") Long id){
+        taskService.delete(id);
+        return ResponseEntity.ok(new ResponseWrapper("Task deleted", HttpStatus.OK));
+    }
+    @GetMapping("/employee/pending-tasks")
+    @RolesAllowed("Employee")
+    public ResponseEntity<ResponseWrapper> employeePendingTasks(){
+        return ResponseEntity.ok(new ResponseWrapper("Pending tasks retrieved", taskService.listAllTasksByStatusIsNot(Status.COMPLETE), HttpStatus.OK));
+    }
+    @PutMapping("/employee/update")
+    @RolesAllowed("Employee")
+    public ResponseEntity<ResponseWrapper> employeeUpdateTasks(@RequestBody TaskDTO task){
+        taskService.update(task);
+        return ResponseEntity.ok(new ResponseWrapper("Task us updated", HttpStatus.OK));
+}
+    @GetMapping ("/employee/archive/")
+    @RolesAllowed("Employee")
+    public ResponseEntity<ResponseWrapper> employeeArchiveTasks(){
+        return ResponseEntity.ok(new ResponseWrapper("Archive tasks", taskService.listAllTasksByStatus(Status.COMPLETE), HttpStatus.OK));
+}
+}
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -20,25 +21,30 @@ public class UserController {
     }
 
     @GetMapping()
+    @RolesAllowed({"Manager", "Admin"})
     public ResponseEntity<ResponseWrapper> getUsers(){
         List<UserDTO> userDTOList=userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("List of Users retrieved",userDTOList, HttpStatus.OK));
     }
     @GetMapping("/{username}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String username){
         return ResponseEntity.ok(new ResponseWrapper("User retrieved successfully", userService.findByUserName(username),HttpStatus.OK));
     }
     @PostMapping()// works with base
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
         userService.save(user);// we do not normally pass object in the body when create and delete
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User created",HttpStatus.CREATED));
     }
     @PutMapping()
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User Updated",HttpStatus.ACCEPTED));
     }
     @DeleteMapping("/{username}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String username){
         userService.delete(username);
         return ResponseEntity.ok().body(new ResponseWrapper("User deleted",HttpStatus.ACCEPTED));
