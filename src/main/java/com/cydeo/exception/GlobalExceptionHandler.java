c032143880f763b256f450aca1b5954a64bc4641
Ekaterina <ekaterina.sementeva@gmail.com>
2023-02-22 23:50:28 -0500

    Line up

--- src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
@@ -4,13 +4,11 @@ import com.cydeo.annotation.DefaultExceptionMessage;
 import com.cydeo.dto.DefaultExceptionMessageDto;
 
 import com.cydeo.dto.ResponseWrapper;
-import com.cydeo.exception.TicketingProjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.HandlerMethod;
@@ -21,20 +19,21 @@ import java.util.Optional;
 @RestControllerAdvice// this class will be executed whenever exception happens
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(TicketingProjectException.class)//whenever exc.happens which belongs to my Custom Exception class, this code will be executed
-    public ResponseEntity<ResponseWrapper> serviceException(TicketingProjectException se){
+    @ExceptionHandler(TicketingProjectException.class)
+//whenever exc.happens which belongs to my Custom Exception class, this code will be executed
+    public ResponseEntity<ResponseWrapper> serviceException(TicketingProjectException se) {
         String message = se.getMessage();
-        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.CONFLICT.value()).message(message).build(),HttpStatus.CONFLICT);
+        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.CONFLICT.value()).message(message).build(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(AccessDeniedException.class)//if Access Denied exception, execute the following
-    public ResponseEntity<ResponseWrapper> accessDeniedException(AccessDeniedException se){
+    public ResponseEntity<ResponseWrapper> accessDeniedException(AccessDeniedException se) {
         String message = se.getMessage();
-        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.FORBIDDEN.value()).message(message).build(),HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.FORBIDDEN.value()).message(message).build(), HttpStatus.FORBIDDEN);
     }
 
     // for the rest, execute the following
-    @ExceptionHandler({Exception    .class, RuntimeException.class, Throwable.class, BadCredentialsException.class})
+    @ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class, BadCredentialsException.class})
     public ResponseEntity<ResponseWrapper> genericException(Throwable e, HandlerMethod handlerMethod) {
 
         Optional<DefaultExceptionMessageDto> defaultMessage = getMessageFromAnnotation(handlerMethod.getMethod());
@@ -49,6 +48,7 @@ public class GlobalExceptionHandler {
         }// else throw this
         return new ResponseEntity<>(ResponseWrapper.builder().success(false).message("Action failed: An error occurred!").code(HttpStatus.INTERNAL_SERVER_ERROR.value()).build(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     private Optional<DefaultExceptionMessageDto> getMessageFromAnnotation(Method method) {
         DefaultExceptionMessage defaultExceptionMessage = method.getAnnotation(DefaultExceptionMessage.class);
         if (defaultExceptionMessage != null) {
